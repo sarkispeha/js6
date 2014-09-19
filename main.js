b5180b4d45d1dc1454176ef644850686f1c87d26
Sarkis Peha <sarkispeha@gmail.com>
2014-09-19 16:36:43 -0600

    auto-inputs working

--- main.js
+++ main.js
@@ -6,37 +6,42 @@ var favbooks = '';
 var favjava = '';
 
 	$(document).on('click', '.user-profile button', function(){
-		console.log('works');
 	 	$(this).text('Hide Form');
 		$('.user-input').toggleClass('user-input-show');
 	})
 
-	$(document).on('click', '.userSubmit', function(e) {
-		e.preventDefault();
-		var user = $('form input[name="username"]').val();
-		var bio = $('form input[name="bio"]').val();
-		var favbooks = $('form input[name="favbooks"]').val();
-		var favjava = $('form input[name="favjava"]').val();
+	// $(document).on('click', '.userSubmit', function(e) {     //submit button
+	// 	e.preventDefault();
+		$(document).on('keyup','input[name="username"]',(function() {	//autosave keyup
+ 		var value = $(this).val();
+ 		$('#username').text(value)
+ 		}))
+ 		
+ 		$(document).on('keyup','input[name="bio"]',(function() {	//autosave keyup
+ 		var value = $(this).val();
+ 		$('#bio').text(value)
+ 		}))
 
-		$('#username').append(user);
-		$('#bio').append(bio);
-		$('#favbooks').append(favbooks);
-		$('#favjava').append(favjava);
-		});
+ 		$(document).on('keyup','input[name="favbooks"]',(function() {	//autosave keyup
+ 		var value = $(this).val();
+ 		$('#favbooks').text(value)
+ 		}))
+
+ 		$(document).on('keyup','input[name="favjava"]',(function() {	//autosave keyup
+ 		var value = $(this).val();
+ 		$('#favjava').text(value)
+ 		}))
 
 
-  
-});
+		// var user = $('form input[name="username"]').val();
+		// var bio = $('form input[name="bio"]').val();
+		// var favbooks = $('form input[name="favbooks"]').val();
+		// var favjava = $('form input[name="favjava"]').val();
 
+		// $('#username').append(user);
+		// $('#bio').append(bio);
+		// $('#favbooks').append(favbooks);
+		// $('#favjava').append(favjava);
+		 
 
-
-
-
-
-// $('input[name="username"]').keyup(function() {
-// 		var value = $(this).val();
-// 		$('p').text(value);
-// 	})
-// 	.keyup();
-
-// 	autosave feature
+})
